2d54182348f46944bfd0de3031e94900ee3cd996
Aaron Alcala <84670853+Alca0004@users.noreply.github.com>
2022-01-18 14:30:36 -0500

    Check1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
+import request from './Request';
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix-Clone</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={request.fetchTrending }/>
+      
     </div>
   );
 }
