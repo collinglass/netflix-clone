2c686dca75cedb1591b0ed82308b663995ec315c
Aaron Alcala <84670853+Alca0004@users.noreply.github.com>
2022-01-18 15:43:13 -0500

    Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
-import request from './Request';
+import request from './request';
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix-Clone</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fetchUrl={request.fetchTrending }/>
-      
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,15 +1,41 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 
-function Row({ title }) {
-    const [movies, setMovies] = useState([]);
+const base_url = 'https://image.tmdb.org/t/p/original/';
 
-    return (
-        <div>
-            <h2>{title}</h2>    
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
 
-            {/* container -> poster */}
-        </div>
-    )
+  //A snipped of code which runs based on a specific condition
+  useEffect(() => {
+    // if [], run once when the row loads, and dont run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.table(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="rows__posters">
+        {/* several row__poster(s) */}
+
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
