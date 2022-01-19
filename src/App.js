c2d42ca6af2cc0d20641e8ac853afe3e2fdd4e29
Aaron Alcala <84670853+Alca0004@users.noreply.github.com>
2022-01-19 15:16:12 -0500

    Check2
    Adding Row.css
    fetching movie genres

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
       <h1>Netflix-Clone</h1>
       <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} />
       <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentariesMovies} />
     </div>
   );
 }
