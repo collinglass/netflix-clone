985213e53778d4f9cc1bcb4b4a0ed12dbb269cd1
Aaron Alcala <84670853+Alca0004@users.noreply.github.com>
2022-01-20 16:23:52 -0500

    Banner
    banner

--- src/Banner.js
+++ src/Banner.js
@@ -27,22 +27,19 @@ function Banner() {
         backgroundSize: 'cover',
         backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
         )`,
+        backgroundPosition: 'center center',
       }}
     >
       <div className="banner__contents">
-        {' '}
-        {/*title */}
         <h1 className="banner__title">
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
+
         <div className='"banner__buttons'>
-          {' '}
-          {/* div > 2 buttons*/}
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{movie?.overview} </h1>{' '}
-        {/* description*/}
+        <h1 className="banner__description">{movie?.overview} </h1>
       </div>
     </header>
   );
