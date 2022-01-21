020bebbd969442005c31d5455e072661cee8937d
Aaron Alcala <84670853+Alca0004@users.noreply.github.com>
2022-01-20 19:54:48 -0500

    Banner Completed

--- src/Banner.js
+++ src/Banner.js
@@ -9,16 +9,22 @@ function Banner() {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(requests.fetchNetflixOriginals);
+
       setMovie(
         request.data.results[
           Math.floor(Math.random() * request.data.results.length - 1) // Request random Movie
         ]
       );
+      return request;
     }
     fetchData();
   }, []);
 
   console.log(movie);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+  }
 
   return (
     <header
@@ -30,7 +36,7 @@ function Banner() {
         backgroundPosition: 'center center',
       }}
     >
-      <div className="banner__contents">
+      <div className="banner__content">
         <h1 className="banner__title">
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
@@ -39,8 +45,13 @@ function Banner() {
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{movie?.overview} </h1>
+
+        <h1 className="banner__description">
+          {movie?.overview}
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
+      <div className="banner--fadeBottom" />
     </header>
   );
 }
